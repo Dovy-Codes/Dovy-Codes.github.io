026f86aa903c8c44baa82f827e3870f027656050
Varstasdp <d.pocius93@gmail.com>
2023-02-18 12:09:02 +0000

    Add bug fix for nav bar everytime I would press the toggle button in mobile it would scroll to the top, needed to change anchor to div

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+
+// In this section we toggle class active and make a nav menu appear in Mobile layouts
 const toggleButton = document.getElementById("toggle-button")
 const navBar = document.getElementsByClassName("nav-links")[0]
 
