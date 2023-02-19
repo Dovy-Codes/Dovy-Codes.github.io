1c66e4850b60e23ba914b995fae77101bfce1400
Varstasdp <d.pocius93@gmail.com>
2023-02-19 11:36:47 +0000

    Add a photo of me and jQuery fadeIn Welcome message

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,20 @@
 
 // In this section we toggle class active and make a nav menu appear in Mobile layouts
-const toggleButton = document.getElementById("toggle-button")
-const navBar = document.getElementsByClassName("nav-links")[0]
+const toggleButton = document.getElementById("toggle-button");
+const navBar = document.getElementsByClassName("nav-links")[0];
 
 toggleButton.addEventListener("click", () => {
     navBar.classList.toggle("active")
 })
+
+// const welcome = ["Hello!", "¡Hola!", "Привет!", "Labas!"];
+// let i = 0;
+// setInterval(Hello, 2000);
+// function Hello() {
+//     if(i >= welcome.length-1) {
+//         i = 0;
+//     } else { i++; }
+//     document.getElementById("hi").innerHTML = welcome[i];
+//     // document.getElementById("hi").innerHTML = welcome[i];
+// }   
+
