0d1592890b0715de7cfbbeafdef93bf1896f5bee
Varstasdp <d.pocius93@gmail.com>
2023-03-03 16:26:28 +0000

    Keeping project in current layout, planning further changes later

--- JS/main.js
+++ JS/main.js
@@ -25,3 +25,51 @@ function openForm(){
     //This makes sure that you can't scroll when the contact form is open
     document.body.setAttribute("style", "height: 100%; overflow-y: hidden;");
 }
+
+const travel = document.getElementById("travel-website");
+    const todo = document.getElementById("todo-app");
+    const git = document.getElementById("github");
+    const thisWeb = document.getElementById("this-website");
+    todo.addEventListener("mouseover", changeWidthTop)
+    todo.addEventListener("mouseout", changeWidthTop);
+    function changeWidthTop(){
+        if ($(document).width() > 1210){
+            if(todo.getAttribute("style") == "width: 65%;"){
+                todo.setAttribute("style", "width: 30%;");
+                travel.setAttribute("style", "width: 65%;");
+            }
+            else{
+            todo.setAttribute("style", "width: 65%;");
+            travel.setAttribute("style", "width: 30%;");
+            }
+        }
+    }
+    git.addEventListener("mouseover", changeWidth)
+    git.addEventListener("mouseout", changeWidth);
+    function changeWidth(){
+        if ($(document).width() > 1210){
+            if(git.getAttribute("style") == "width: 65%;"){
+                git.setAttribute("style", "width: 30%;");
+                thisWeb.setAttribute("style", "width: 65%;");
+            }
+            else{
+            git.setAttribute("style", "width: 65%;");
+            thisWeb.setAttribute("style", "width: 30%;");
+            }
+        }
+    }
+
+    //Didn't use this feature in the end but keeping it for now it changes text every 2.5s
+    /* 
+    <script type="text/javascript">
+    (function () {
+        let welcome = ["Hi!", "¡Hola!", "Привет!", "Labas!"],
+        i = 0
+        setInterval(function(){
+            $("#hi").fadeOut(function(){
+                $(this).html(welcome[(i =(i + 1) % welcome.length)]).fadeIn();
+            });
+        }, 2500)
+    }) ();
+    </script>
+    */
